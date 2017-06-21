59eb28c5a44531a7162749dabc5979b1cdfc9a03
Eliza H <eliza@scripted.org>
2017-06-21 15:41:41 +0000

    commit all eliza's page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+var pics =["ElizaFlorence.jpg","ElizaFrance.jpg","Elizaitaly.jpg","Elizasf.jpg","Elizawework.JPG","IMG_0053.jpg","IMG_8176.JPG"]
+$("button").click(function(){
+    var random = Math.floor(Math.random()*pics.length);
+    $("#photo").append("<img src=photos/" +pics[random]+">");
 
-
+});
 });
